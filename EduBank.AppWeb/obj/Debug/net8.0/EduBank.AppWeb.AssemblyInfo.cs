993c49b8f226c41a58767ef44d2aafca2c156f53
Chris Patrick <cchilonsegura@gmail.com>
2025-11-12 13:36:57 -0500

    Actulización de ChatBot 2

--- EduBank.AppWeb/obj/Debug/net8.0/EduBank.AppWeb.AssemblyInfo.cs
+++ EduBank.AppWeb/obj/Debug/net8.0/EduBank.AppWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduBank.AppWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6509a03778df694d593fc5b72a1270d809acca02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b459f40546655c436846bbfb161fd845d9ac37c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduBank.AppWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduBank.AppWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
